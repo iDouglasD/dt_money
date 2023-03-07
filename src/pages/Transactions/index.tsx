6ef58252584e8f4b6aac518768f14fafaf00996e
Douglas Duarte <douglasduarte.eng@gmail.com>
2023-03-07 06:40:08 -0300

    style: Transactions table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from './styles'
 import './styles.ts'
 
 export function Transactions() {
@@ -7,6 +8,34 @@ export function Transactions() {
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant='income'>
+                  R$ 12.000,00
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>07/03/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Aluguel</td>
+              <td>
+                <PriceHighlight variant='outcome'>
+                  - R$ 590,00
+                </PriceHighlight>
+              </td>
+              <td>Moradia</td>
+              <td>01/03/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+
     </div>
   )
 }
