3e8e00fad85dd56ce67faaf0d8c6dd6fc383b7be
Douglas Duarte <douglasduarte.eng@gmail.com>
2023-03-07 06:54:37 -0300

    style: Search form

---
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -0,0 +1,11 @@
+import { SearchFormContainer } from "./styles";
+import { MagnifyingGlass } from 'phosphor-react'
+
+export function SearchForm() {
+  return <SearchFormContainer>
+    <input type="text" placeholder="Busque por transações" />
+    <button type="submit">
+      <MagnifyingGlass />
+    </button>
+  </SearchFormContainer>
+}
