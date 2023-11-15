4d72eb66aa0e9aef2123335e54b2bae4ba35b1d4
Douglas Duarte <douglasduarte.eng@gmail.com>
2023-11-15 10:58:33 -0300

    change: custom hook to calculate summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,27 +1,10 @@
 import { SummaryCard, SummaryContainer } from "./styles";
 import { ArrowCircleUp, CurrencyDollar, ArrowCircleDown } from 'phosphor-react'
-import { useTransactions } from "../../hooks/useTransactions";
 import { priceFormatter } from "../../utils/formatter";
+import { useSummary } from "../../hooks/useSummary";
 
 export function Summary() {
-  const { transactions } = useTransactions()
-
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0
-    })
+  const summary = useSummary();
 
   return <SummaryContainer>
     <SummaryCard>
